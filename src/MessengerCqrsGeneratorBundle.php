88e4da2a8b2145639e96acbd97da71bf76208d8f
Alex <alexandre.bailly@golem.ai>
2022-05-21 20:35:04 +0200

    init

--- src/MessengerCqrsGeneratorBundle.php
+++ src/MessengerCqrsGeneratorBundle.php
@@ -2,13 +2,8 @@
 
 namespace Kodmit\MessengerCqrsGeneratorBundle;
 
-use Kodmit\MessengerCqrsGeneratorBundle\DependencyInjection\MessengerCqrsGeneratorExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MessengerCqrsGeneratorBundle extends Bundle
 {
-    public function getContainerExtension(): MessengerCqrsGeneratorExtension
-    {
-        return new MessengerCqrsGeneratorExtension();
-    }
 }
