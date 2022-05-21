6620cec95ac499ef40cea2d0c222021f6353cbfd
Alex <alexandre.bailly@golem.ai>
2022-05-21 20:36:26 +0200

    init

---
+++ src/DependencyInjection/MessengerCqrsGeneratorExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kodmit\MessengerCqrsGeneratorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class MessengerCqrsGeneratorExtension extends Extension implements ExtensionInterface
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
--- src/MessengerCqrsGeneratorBundle.php
+++ src/MessengerCqrsGeneratorBundle.php
@@ -2,8 +2,13 @@
 
 namespace Kodmit\MessengerCqrsGeneratorBundle;
 
+use Kodmit\MessengerCqrsGeneratorBundle\DependencyInjection\MessengerCqrsGeneratorExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MessengerCqrsGeneratorBundle extends Bundle
 {
+    public function getContainerExtension(): MessengerCqrsGeneratorExtension
+    {
+        return new MessengerCqrsGeneratorExtension();
+    }
 }
